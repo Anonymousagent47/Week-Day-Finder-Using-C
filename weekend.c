95d25b5f3f8aef934ee7d344905d7cd6a88af647
Prince Solanki <87561928+Anonymousagent47@users.noreply.github.com>
2022-09-13 18:59:06 +0530

    Update weekend.c

--- weekend.c
+++ weekend.c
@@ -23,3 +23,32 @@ int main()
     }
     return 0;
 }
+
+//To Do it in If..Else Statement
+
+
+#include<stdio.h>
+int main()
+{
+    int day;
+    printf("Enter Day (1-7):");
+    scanf("%d",&day);
+    if(day==1){
+        printf("Sunday");
+    }else if(day==2){
+        printf("Monday");
+    }else if(day==3){
+        printf("Tuesday");
+    }else if(day==4){
+        printf("Wednesday");
+    }else if(day==5){
+        printf("thursday");
+    }else if(day==6){
+        printf("Friday"):
+    }else if(day==7){
+        printf("Saturday");
+    }else{
+        printf("Enter Valid Number !");
+    }
+    return 0;
+}
